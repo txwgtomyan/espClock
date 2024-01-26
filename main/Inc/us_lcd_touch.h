e34ad7fe90ef15a67ae4eac8d1894df449bb3a29
杨溢 <121284031@qq.com>
2024-01-26 08:03:27 +0800

    同步用

---
+++ main/Inc/us_lcd_touch.h
@@ -0,0 +1,7 @@
+#ifndef __US_LCD_TOUCH_H_
+#define __US_LCD_TOUCH_H_
+
+
+void us_lcd_touch(void);
+
+#endif
--- main/app_main.c
+++ main/app_main.c
@@ -17,6 +17,7 @@
 #include "us_spiffs.h"
 #include "us_nvs.h"
 #include "us_rgb_lcd.h"
+#include "us_lcd_touch.h"
 /* 日志打印头文件和代码 */
 #include "esp_log.h"
 static const char *TAG = "MAIN APP";
@@ -71,7 +72,8 @@ void app_main(void)
     // us_timer_init();
     //us_spiffs_init();
     //us_nvs_init();
-    us_rgb_lcd_init();
+    //us_rgb_lcd_init();
+    us_lcd_touch();
     for (;;)
     {
         /* 死循环 */
